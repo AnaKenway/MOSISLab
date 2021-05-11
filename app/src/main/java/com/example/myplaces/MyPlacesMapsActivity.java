8524137ab99a72f6e57aa2f652fd4eb7381a5d77
AnaKenway <ancemilenkovic@gmail.com>
2021-05-11 19:15:04 +0200

    Manje izmene
    Izmenjen edit_my_place.xml

--- app/src/main/java/com/example/myplaces/MyPlacesMapsActivity.java
+++ app/src/main/java/com/example/myplaces/MyPlacesMapsActivity.java
@@ -23,6 +23,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+
 public class MyPlacesMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
